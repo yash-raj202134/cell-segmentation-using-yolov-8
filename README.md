b67c498fae8b95dd28084f6d51970eea66df833d
yash-raj202134 <yashraj3376@gmail.com>
2024-04-10 19:20:03 +0530

    files setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cell-segmentation-using-yolov-8
 Cell-Segmentation-Using-Yolo-v8
+
+
+## Workflows
+
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Yash Raj',
+    author_email= 'yashraj3376@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
