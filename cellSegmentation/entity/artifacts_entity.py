afa6caf4c9a5ef8cb8ea0ab7b75633f43693ddd8
yash-raj202134 <yashraj3376@gmail.com>
2024-04-11 18:40:07 +0530

    model trainer

--- cellSegmentation/entity/artifacts_entity.py
+++ cellSegmentation/entity/artifacts_entity.py
@@ -6,3 +6,15 @@ class DataIngestionArtifact:
     feature_store_path:str
 
 
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+
+
+
--- main.py
+++ main.py
@@ -7,13 +7,46 @@ import os
 from cellSegmentation.pipeline.training_pipeline import TrainPipeline
 
 
+# Testing
+
+# STAGE_NAME = "Data ingestion stage"
+# try:
+#     logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#     obj = TrainPipeline()
+#     data_ingestion_art = obj.start_data_ingestion()
+#     logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#     raise AppException(e,sys)
 
 
-STAGE_NAME = "Data ingestion stage"
-try:
-    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
+# STAGE_NAME = "Data Validation stage"
+
+# try:
+#     logging.info(f">>>>>> stage {STAGE_NAME} started<<<<<<")
+#     data_validation = TrainPipeline()
+
+#     data_validation.start_data_validation(data_ingestion_art)
+#     logging.info(f">>>>>> stage {STAGE_NAME} completed<<<<<<\n\nx==============x")
+
+# except Exception as e:
+#     logging.exception(e)
+#     raise AppException(e,sys)
+
+
+
+# STAGE_NAME = "Model trainer stage"
+
+# try:
+#     logging.info(f">>>>>> stage {STAGE_NAME} started<<<<<<")
+#     model_trainer = ModelTrainerTrainingPipeline()
+#     model_trainer.main()
+#     logging.info(f">>>>>> stage {STAGE_NAME} completed<<<<<<\n\nx==============x")
+
+# except Exception as e:
+#     raise AppException(e,sys)
+
+if __name__ =="__main__":
+
     obj = TrainPipeline()
-    obj.start_data_ingestion()
-    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-    raise AppException(e,sys)
+    obj.run_pipeline()
